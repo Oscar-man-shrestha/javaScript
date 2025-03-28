5a0af1fc0f3dd232b1474b51b474ab1c6b3238b3
Oscar-man-shrestha <oscarmanshrestha@gmail.com>
2025-03-28 22:10:54 +0530

    javaScript

---
+++ script.js
@@ -0,0 +1,38 @@
+const mark = {
+    fullName:"Mark Miller",
+    mass : 300,
+    height : 260,
+
+    calcBmI : function(){
+     this.bmi = this.mass/this.height **2;
+     return this.bmi;
+    },
+};
+
+const john = {
+    fullName:"John Smith",
+    mass : 400,
+    height : 360,
+
+    calcBmI : function(){
+     this.bmi= this.mass/this.height **2;
+     return this.bmi;
+    },
+};
+
+mark.calcBmI();
+
+
+john.calcBmI();
+
+
+
+
+if(john.bmi>mark.bmi){
+        console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})  `)
+    }
+    else
+    {
+        console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})  `)
+    
+    }
